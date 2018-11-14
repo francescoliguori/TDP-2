4692602f144aef6af81f96bc20eb04af0b94f874
francescoliguori <41292339+francescoliguori@users.noreply.github.com>
2018-11-14 18:12:40 +0100

    ex.2 definition of methods
    ex.2 definition of methods

---
+++ Statistics.py
@@ -0,0 +1,31 @@
+from NewAVLTreeMap import *
+
+
+class Statistics():
+
+    def __init__(self,file_input):
+        avl=NewAVLTreeMap();
+        try:
+            file=open(file_input,"r")
+        except:
+            print("error in opening the file ")
+
+
+    def add(self,k,v):
+
+    def len(self):
+
+    def occurrencese(self):
+
+    def average(self):
+
+    def median(self):
+
+    def percentile(self,j=20):
+
+    def mostFrequent(self,j):
+
+
+
+
+
