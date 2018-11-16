7b3ddba241b84cb5322b73bb2a33db338ba9ac63
francescoliguori <41292339+francescoliguori@users.noreply.github.com>
2018-11-16 15:23:14 +0100

    add file verifica ex4
    used assert

---
+++ verificaex4.py
@@ -0,0 +1,37 @@
+import unittest
+from circularSubstring import *
+
+
+STRING="ciaomondo"
+
+class TestProjectEx4(unittest.TestCase):
+    print("stringa in cui cercare :",STRING)
+    print("")
+
+    def test_substring_normal(self):
+        print("----test substring normale----")
+        substring="c"
+        print("substring : ",substring)
+        self.assertTrue(circular_substring(substring,STRING))
+        print("la sottostringa è presente")
+
+    def test_substring_0(self):
+        print("----test substring lunghezza 0 ----")
+        substring=""
+        print("substring : ",substring)
+        self.assertTrue(circular_substring(substring,STRING))
+        print("la sottostringa è presente")
+
+    def test_nosubstring(self):
+        print("----test substring non presente ----")
+        substring="test"
+        print("substring : ",substring)
+        self.assertFalse(circular_substring(substring,STRING))
+        print("la sottostringa non è presente")
+
+    def test_circular_substring(self):
+        print("----test cricular substring  ----")
+        substring="ocia"
+        print("substring : ",substring)
+        self.assertTrue(circular_substring(substring,STRING))
+        print("la sottostringa è presente")
