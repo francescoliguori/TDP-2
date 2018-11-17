ca7f417db6cd9a04374c32bae0cb6ddfa160ae8c
francescoliguori <41292339+francescoliguori@users.noreply.github.com>
2018-11-17 14:10:25 +0100

    verification file ex3
    included the folders that are tested

---
+++ verificaEx3.py
@@ -0,0 +1,27 @@
+import unittest
+from find_repetition import *
+
+
+class TestProjectEx3(unittest.TestCase):
+
+    def test_differentFile(self):
+        print("---test tutti file differenti---")
+        result=find_repetition("./differentfile/")
+        print(result)
+        self.assertEqual(result,[])
+
+    def test_sameFile(self):
+        print("---test tutti file uguali---")
+        result=find_repetition("./samefile/")
+        print(result)
+        self.assertEqual(result,['prova3.txt', 'prova4 - Copia.txt', 'prova4.txt'])
+
+    def test_mixedFile(self):
+        print("---test file differenti ed uguali---")
+        result=find_repetition("./mixedfile/")
+        print(result)
+        self.assertEqual(result,['prova1.txt', 'prova2.txt', 'prova4.txt'])
+
+if __name__ == "__main__":
+    unittest.main()
+
