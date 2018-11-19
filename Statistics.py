c208bd81b45d26ab48ff43b5880cff823f964210
giodice <38973295+giodice@users.noreply.github.com>
2018-11-19 11:16:19 +0100

    Aggiornamento CRsid
    - Modifiche a Statistics: tolti commenti, migliorato stile e chiamate a funzione.
    - inseriti data_set_corrotto e data_test_test utili  nel file verifica_2
    - verifica_2
    - unicum data_set

--- Statistics.py
+++ Statistics.py
@@ -5,40 +5,30 @@ class Statistics:
 
     def __init__(self, file_input):
         self.avl = NewAVLTreeMap()
-        self._file_input = file_input  # questo attributo lo teniamo nel caso in cui nel metodo add bisogna aggiornare il file (è un dubbio)
-        # with open(file_input, "r") as file:  #lo statement with ci chiude il file automaticamente
-        #     line = file.readline()  # readline legge una riga volta per volta
-        #     while line:  # fino a quando la riga non è none
-        #         lista = line.split(" ")  # prende gli elementi separati dallo spazio e li mette in una lista, i valori sono stringhe
-        #         p = self.avl.find_position(int(lista[0]))  # controlla se la chiave è nell'albero
-        #         if p is not None and p.key() == int(lista[0]): # se p non ha trovato la chiave o ritorna None oppure il suo vicino(motivo della seconda condizione)
-        #             self.avl[int(lista[0])][0] += 1  # aumenta di uno le occorrenze
-        #             self.avl[int(lista[0])][1] += int(lista[1])  #somma a total il value
-        #         else:
-        #             self.avl[int(lista[0])] = [1, int(lista[1])]  # crea un nodo con occorrenza a 1 e total pari al valore
-        #         line = file.readline()  # passa alla riga successiva
+        # self._file_input = file_input  # questo attributo lo teniamo nel caso in cui nel metodo add bisogna aggiornare il file (è un dubbio)
         self._popola_albero(file_input)
 
 
     def _popola_albero(self,file_input):
-        #questa funzione popola l'albero con gli elementi del dataset. Se nella add non sarà necessario agiornare il dataset allora si
-        #potrà rifinire ulteriormente il codice chiamando nel while line la funzione add
+        #  questa funzione popola l'albero con gli elementi del dataset. Se nella add non sarà necessario agiornare il dataset allora si
+        #  potrà rifinire ulteriormente il codice chiamando nel while line la funzione add
         try:
             with open(file_input, "r") as file:  # lo statement with ci chiude il file automaticamente
                 line = file.readline()  # readline legge una riga volta per volta
                 while line:  # fino a quando la riga non è none
-                    lista = line.split(" ")  # prende gli elementi separati dallo spazio e li mette in una lista, i valori sono stringhe
-                    p = self.avl.find_position(int(lista[0]))  # controlla se la chiave è nell'albero
-                    if p is not None and p.key() == int(lista[0]):  # se p non ha trovato la chiave o ritorna None oppure il suo vicino(motivo della seconda condizione)
-                        self.avl[int(lista[0])][0] += 1  # aumenta di uno le occorrenze
-                        self.avl[int(lista[0])][1] += int(lista[1])  # somma a total il value
-                    else:
-                        self.avl[int(lista[0])] = [1, int(lista[1])]  # crea un nodo con occorrenza a 1 e total pari al valore
+                    lista = line.split(" ")  #  prende gli elementi separati dallo spazio e li mette in una lista, i valori sono stringhe
+                    if len(lista) != 2:
+                        raise ValueError("File Corrotto.")
+                    self.add(int(lista[0]), int(lista[1]))
                     line = file.readline()  # passa alla riga successiva
-        except:
-            print("impossibile aprire il file. Controllare che il nome e/o path sia corretto.")
+        except ValueError as e:
+            print("File Corrotto.")
+        except FileNotFoundError as e:
+            print("Impossibile aprire il file. Controllare che il nome e/o path sia corretto.")
 
     def add(self, k, v):
+        if not isinstance(k, int) or not isinstance(v, int):
+            raise TypeError("Il tipo di k e/o v non è valido")
         p = self.avl.find_position(k)  # controlla se la chiave è nell'albero
         if p is not None and p.key() == k:  # se p non ha trovato la chiave o ritorna None oppure il suo vicino(motivo della seconda condizione)
             self.avl[k][0] += 1  # aumenta di uno le occorrenze
@@ -47,18 +37,17 @@ class Statistics:
             self.avl[k] = [1, v]  # crea un nodo con occorrenza a 1 e total pari al valore
 
         # questo codice aggiorna anche il file, come ho detto prima, credo che non si deve fare
-        with open(self._file_input, 'a') as file:  # 'a' aggiorna il file scrivendo una nuova riga alla fine
-
-            file.write("\n{} {}".format(k, v))  # .format sostituisce alle {} il valore che gli abbiamo passato
-
+        # with open(self._file_input, 'a') as file:  # 'a' aggiorna il file scrivendo una nuova riga alla fine
+        #
+        #     file.write("\n{} {}".format(k, v))  # .format sostituisce alle {} il valore che gli abbiamo passato
 
     def len(self):
         return len(self.avl)
 
-    def get_frequency(self,p):
+    def _get_frequency(self, p):
         return p.value()[0]
 
-    def get_total(self,p):
+    def _get_total(self, p):
         return p.value()[1]
 
     def occurrences(self):
@@ -67,39 +56,39 @@ class Statistics:
             raise Exception("Il dataset è vuoto")
         occorrenze = 0
         for p in self.avl.preorder():
-            occorrenze += self.get_frequency(p)
+            occorrenze += self._get_frequency(p)
             #occorrenze += e.value()[0]
         return occorrenze
 
     def average(self):
         """Restituisce la media dei valori di tutte le occorrenze presenti nel dataset"""
         #media dei valori = totale dei valori / totale delle occorrenze
+
         if self.len() == 0:
-            raise Exception("La media è 0 perché il dataset è vuoto")
+            raise Exception("Il dataset è vuoto")
 
         complessivo = 0
         for p in self.avl.preorder():
-            complessivo += self.get_total(p)
+            complessivo += self._get_total(p)
         return complessivo/self.occurrences()
 
     def median(self):
         """ se il numero n di dati è dispari la mediana corrisponde al valore centrale,
             ovvero al valore che occupa la posizione ( n + 1 ) / 2.
-
             se il numero  n di dati è pari, la mediana è stimata utilizzando i due valori
             che occupano le posizioni ( n / 2 ) e ( ( n / 2 ) + 1 )
             :return: il valore della mediana"""
-        # entries = 0
-        # for key in self.avl.keys():
-        #     entries += self.avl[key][0]
-        # freq = 0
-        # for node in self.avl.inorder():
-        #     freq += node.value()[0]
-        #     if freq >= entries / 2:
-        #         return node.key()
+        if self.len() == 0:
+            raise Exception("Il dataset è vuoto")
         return self.percentile(50)
 
     def percentile(self, j=20):
+        if self.len() == 0:
+            raise Exception("Il dataset è vuoto")
+        if not isinstance(j, int):
+            raise TypeError("Il tipo di j non è valido")
+        if j < 0 or j > 100:
+            raise ValueError("Il valore di j non è valido")
         entries = 0
         for key in self.avl.keys():
             entries += self.avl[key][0]
@@ -110,22 +99,15 @@ class Statistics:
                 return node.key()
 
     def mostFrequent(self, j):
-        lista = {}
+        if self.len() == 0:
+            raise Exception("Il dataset è vuoto")
+        if not isinstance(j, int):
+            raise TypeError("Il tipo di j non è valido")
+        if j < 0 or j > self.len():
+            raise ValueError("Il valore di j non è valido")
+
+        dict_frequency = {}
         for e in self.avl.preorder():
-            print(e.key())
-            lista[e.key()] = e.value()[0]
-        lista = sorted(lista, key=lista.getitem, reverse=True)
+            dict_frequency[e.key()] = e.value()[0]
+        lista = sorted(dict_frequency, key=dict_frequency.__getitem__, reverse=True)
         return lista[0:j]
-
-
-if __name__ == "__main__":
-    stat = Statistics("data_set.txt")
-    for e in stat.avl.preorder():
-        print(e.key(), " ", e.value())
-
-    print(stat.occurrences())
-    print(stat.average())
-
-    # stat.add(4, 5)
-    # for e in stat.avl.preorder():
-    #     print(e.key(), " ", e.value())
---
+++ verifica_2.py
@@ -0,0 +1,191 @@
+from Statistics import Statistics
+import unittest
+
+
+class TestStatics(unittest.TestCase):
+    # def test_popola_albero_da_file(self):
+    #     # with self.assertRaises(FileNotFoundError):
+    #     #     stat = Statistics("Path_errato")
+    #
+    #     with self.assertRaises(ValueError, Statistics("data_set_corrotto")) as e:
+    #         print("Eccezione scattata")
+    #         # stat = Statistics("data_set_corrotto.txt")
+    #         #self.assertEqual("File Corrotto.", e.msg)
+    #
+    #     stat = Statistics("data_set.txt")
+    #     for e in stat.avl.preorder():
+    #         print(e.key(), " ", e.value())
+
+    def test_add(self):
+        print("TEST add()\n")
+        stat = Statistics("data_set_test.txt")
+        with self.assertRaises(TypeError):
+            stat.add("ciao", 5)
+            stat.add("ciao", "ciao")
+            stat.add(5, "ciao")
+
+        print("\n\nAggiungo 5, 10\n")
+        stat.add(5, 10)
+        for e in stat.avl.preorder():
+            print(e.key(), " ", e.value())
+
+        print("\n\nAggiungo 54, 24\n")
+        stat.add(54, 24)
+        for e in stat.avl.preorder():
+            print(e.key(), " ", e.value())
+
+        print("\n\nAggiungo 54, 16\n")
+        stat.add(54, 16)
+        for e in stat.avl.preorder():
+            print(e.key(), " ", e.value())
+
+    def test_occurrences(self):
+        print("TEST occurrences()\n")
+        stat = Statistics("data_set_test.txt")
+
+        for e in stat.avl.preorder():
+            print(e.key(), " ", e.value())
+
+        self.assertEqual(stat.occurrences(), 10)
+        print("\nOccorrenze: ", stat.occurrences())
+
+        print("\n\nAggiungo 54, 16\n")
+        stat.add(54, 16)
+        for e in stat.avl.preorder():
+            print(e.key(), " ", e.value())
+
+        self.assertEqual(stat.occurrences(), 11)
+        print("\nOccorrenze: ", stat.occurrences())
+
+        print("\n\nAggiungo 13, 10\n")
+        stat.add(13, 10)
+        for e in stat.avl.preorder():
+            print(e.key(), " ", e.value())
+        self.assertEqual(stat.occurrences(), 12)
+        print("\nOccorrenze: ", stat.occurrences())
+
+    def test_average(self):
+        print("TEST average()\n")
+        stat = Statistics("data_set_test.txt")
+        self.assertEqual(10.8, stat.average())
+        print("Media: ", stat.average())
+
+        print("\nAggiungo 54, 16\n")
+        stat.add(54, 16)
+        self.assertEqual(11.272727272727273, stat.average())
+        print("Media: ", stat.average())
+
+        print("\nAggiungo 13, 10\n")
+        stat.add(13, 10)
+        self.assertEqual(11.166666666666666, stat.average())
+        print("Media: ", stat.average())
+
+    def test_median(self):
+        print("TEST median()\n")
+        stat = Statistics("data_set_test.txt")
+        self.assertEqual(4, stat.median())
+        print("Mediana: ", stat.median())
+
+        print("\nAggiungo 54, 16\n")
+        stat.add(54, 16)
+        self.assertEqual(4, stat.median())
+        print("Mediana: ", stat.median())
+
+        print("\nAggiungo 13, 10\n")
+        stat.add(13, 10)
+        self.assertEqual(4, stat.median())
+        print("Mediana: ", stat.median())
+
+        print("\nAggiungo 2, 4\n")
+        stat.add(2, 4)
+
+        self.assertEqual(4, stat.median())
+        print("Mediana: ", stat.median())
+
+        print("\nAggiungo 2, 4\n")
+        stat.add(2, 4)
+
+        self.assertEqual(4, stat.median())
+        print("Mediana: ", stat.median())
+
+        print("\nAggiungo 2, 4\n")
+        stat.add(2, 4)
+
+        self.assertEqual(4, stat.median())
+        print("Mediana: ", stat.median())
+
+        print("\nAggiungo 2, 4\n")
+        stat.add(2, 4)
+
+        self.assertEqual(3, stat.median())
+        print("Mediana: ", stat.median())
+
+    def test_percentile(self):
+        stat = Statistics("data_set_test.txt")
+        with self.assertRaises(ValueError):
+            percentile = stat.percentile(-1)
+            percentile = stat.percentile(101)
+
+        with self.assertRaises(TypeError):
+            percentile = stat.percentile("ciao")
+            percentile = stat.percentile(50.5)
+
+        self.assertEqual(1, stat.percentile(0))
+        print("Percentile a 0: ", stat.percentile(0))
+
+        self.assertEqual(1, stat.percentile(10))
+        print("Percentile a 10: ", stat.percentile(10))
+
+        self.assertEqual(3, stat.percentile())
+        print("Percentile a 20: ", stat.percentile())
+
+        self.assertEqual(3, stat.percentile(30))
+        print("Percentile a 30: ", stat.percentile(30))
+
+        self.assertEqual(3, stat.percentile(40))
+        print("Percentile a 40: ", stat.percentile(40))
+
+        self.assertEqual(4, stat.percentile(50))
+        print("Percentile a 50: ", stat.percentile(50))
+
+        self.assertEqual(4, stat.percentile(60))
+        print("Percentile a 60: ", stat.percentile(60))
+
+        self.assertEqual(5, stat.percentile(70))
+        print("Percentile a 70: ", stat.percentile(70))
+
+        self.assertEqual(8, stat.percentile(80))
+        print("Percentile a 80: ", stat.percentile(80))
+
+        self.assertEqual(13, stat.percentile(90))
+        print("Percentile a 90: ", stat.percentile(90))
+
+        self.assertEqual(15, stat.percentile(100))
+        print("Percentile a 100: ", stat.percentile(100))
+
+    def test_most_frequent(self):
+        stat = Statistics("data_set_test.txt")
+        with self.assertRaises(ValueError):
+            most_frequent = stat.mostFrequent(-1)
+            most_frequent = stat.mostFrequent(stat.len() + 1)
+
+        with self.assertRaises(TypeError):
+            most_frequent = stat.mostFrequent(5.3)
+            most_frequent = stat.mostFrequent("ciao")
+
+        self.assertEqual("[]", stat.mostFrequent(0).__str__())
+        print("Most frequent 0: ", stat.mostFrequent(0))
+
+        self.assertEqual("[3]", stat.mostFrequent(1).__str__())
+        print("Most frequent 1: ", stat.mostFrequent(1))
+
+        self.assertEqual("[3, 4, 1, 13, 8, 5]", stat.mostFrequent(stat.len() - 1).__str__())
+        print("Most frequent {} : ".format(stat.len() - 1), stat.mostFrequent(stat.len() - 1))
+
+        self.assertEqual("[3, 4, 1, 13, 8, 5, 15]", stat.mostFrequent(stat.len()).__str__())
+        print("Most frequent {} : ".format(stat.len()), stat.mostFrequent(stat.len()))
+
+
+
+if __name__ == "__main__":
+    unittest.main()
