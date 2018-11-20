359ab51766c325a94d4a9026afcf69ea0063f162
francescoliguori <41292339+francescoliguori@users.noreply.github.com>
2018-11-20 19:00:35 +0100

    updated ex4

--- circularSubstring.py
+++ circularSubstring.py
@@ -1,60 +1,15 @@
-import filecmp
+from TdP_collections.text.find_kmp import *
 
-def circular_substring(P,T):
-    m=len(P)
-    tmp=T+T[0:m-1]  #add
-    #print(tmp)
-    result=find_kmp(tmp,P)
-    #print(result)
-    if result==-1:
+
+def circular_substring(p, t):
+    result = find_kmp(t+t[:len(p)-1], p)
+    # print(t+t[:len(p)])
+
+    if result == -1:
         return False
-    #elif result==-1:
-       # return False
     else:
         return True
 
 
-def find_kmp(T, P):
-    '''Return the lowest index of T at which substring P begins (or else -1).'''
-    n, m = len(T), len(P) # introduce convenient notations
-    if m == 0:
-        return 0 # trivial search for empty string
-    fail = compute_kmp_fail(P) # rely on utility to precompute
-    j = 0 # index into text
-    k = 0 # index into pattern
-    while j<n  :
-        if T[j] == P[k]: # P[0:1+k] matched thus far  modified
-            if k == m - 1: # match is complete
-                return j-m+1
-            j += 1 # try to extend match
-            k += 1
-        elif k > 0:
-            k = fail[k-1] # reuse suffix of P[0:k]
-        else:
-            j += 1
-    return -1 # reached end without match
-
-
-
-
-def compute_kmp_fail(P):
-    '''Utility that computes and returns KMP fail list.'''
-    m = len(P)
-    fail = [0]*m # by default, presume overlap of 0 everywhere
-    j = 1
-    k = 0
-    while j < m: # compute f(j) during this pass, if nonzero
-        if P[j] == P[k]: # k + 1 characters match thus far
-            fail[j] = k + 1
-            j += 1
-            k += 1
-        elif k > 0: # k follows a matching prefix
-            k = fail[k-1]
-        else: # no match found starting at j
-            j += 1
-    return fail
-
-if __name__=="__main__":
-    print("test testi")
-    print(find_kmp("ciaomondo","c"))
-
+if __name__ == "__main__":
+    print("circular", circular_substring("ciaomondo", "ocia"))
